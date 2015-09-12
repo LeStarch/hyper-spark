e9482c472d492bc46bb0cd395453529e1f49557f
M Starch <lestarch@googlemail.com>
2015-09-11 19:55:40 -0500

    lestarch: cheanged install location, and fixed netty dependencies

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-mvn clean; mvn install && tar -xzf target/hyper-spark-0.1-dist.tar.gz -C target
+rm -r ../hyper-spark-0.1; mvn clean; mvn install && tar -xzf target/hyper-spark-0.1-dist.tar.gz -C ..
